41f8779aee42d269bb4a60be1463383abf10da11
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-06-07 01:23:19 +0530

    schema with compare passwords

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,7 +8,8 @@ export interface UserDocument extends mongoose.Document{
     name: string,
     password: string,
     createdAt: Date,
-    updatedAt: Date
+    updatedAt: Date,
+    comparePassword(candidatePassword:string):Promise<boolean>
 }
 
 const userSchema = new mongoose.Schema({
@@ -20,15 +21,27 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 })
 
-userSchema.pre("save", async function(next: mongoose.HookNextFunction){
+userSchema.pre("save", async function(next){
     let user = this as UserDocument
 
     if(!user.isModified("password")){
         return next()
     }
 
-    const salt = await bcrypt.genSalt()
+    const salt = await bcrypt.genSalt(config.get<number>('saltWorkFactor'))
+
+    const hash = await bcrypt.hashSync(user.password, salt)
+
+    user.password = hash
+
+    return next()
 })
+
+userSchema.methods.comparePassword  = async function(candidatePassword:string):Promise<boolean>{
+    const user = this as UserDocument
+
+    return bcrypt.compare(candidatePassword, user.password).catch((e)=>  false)
+}
 
 const UserModel = mongoose.model("User", userSchema)
 
